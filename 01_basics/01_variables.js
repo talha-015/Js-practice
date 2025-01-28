8a6bf40eec8c604f1cbd46a71662f3de6156ab2c
talha-015 <muhammdtalha015@gmail.com>
2025-01-28 22:28:52 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 112233
+let accountEmail = "talhactn93@gmail.com"
+var accountPasword = "123456"
+accountCity = "bahawalpur"
+let accountState;
+
+ 
+
+console.log("accountId");
+
+accountEmail = "abc@gmailcom"
+accountPasword = "000000"
+accountCity = "chishtian"
+
+console.table([accountId, accountEmail, accountPasword, accountCity, accountState])
+
+
+
+
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02-datatypes.js
@@ -0,0 +1,21 @@
+     "use strict"; //treat all js code as newer version
+
+    //  console.log(3 + 3); code readability should be high.
+     
+    //  alert(3+3) we are using node js,not browser
+let name = "talha"
+let age =20
+let isLoggedIn = false
+let state = null
+
+
+// number => 2 to power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// symbol => unique
+
+// object
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
