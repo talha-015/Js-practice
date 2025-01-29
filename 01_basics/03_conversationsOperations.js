97f01f369048ba89d8406a4edaea984bdf2bd413
talha-015 <muhammdtalha015@gmail.com>
2025-01-29 19:41:43 +0000

    converstions of datatypes

---
+++ 01_basics/03_conversationsOperations.js
@@ -0,0 +1,29 @@
+ let score = false
+
+ //console.log(typeof score);
+ //console.log(typeof(score));
+ 
+ let valueInNumber = Number(score)
+ //console.log(typeof valueInNumber);
+ //console.log(valueInNumber);
+
+
+
+
+ //"50" => 33
+ // "50abc" => NaN
+ // in boolean like true  =>1; false => 0
+ // when we use 1 means that => true, 0 => false
+ // empty strings "" => false
+ // when we add any value like "talha" => true
+
+
+//let isLoggedIn = 1
+//let booleanIsloggedIn = Boolean(isLoggedIn)
+//console.log( booleanIsloggedIn);
+
+
+let someNumber = 50
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
