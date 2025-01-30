d0678460dee7557f0f0329754bfdee3984c5e356
talha-015 <muhammdtalha015@gmail.com>
2025-01-30 19:59:44 +0000

    conversationsOperations

--- 01_basics/03_conversationsOperations.js
+++ 01_basics/03_conversationsOperations.js
@@ -25,5 +25,42 @@
 
 let someNumber = 50
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+
+// ********************** operations **********************
+
+
+// let value = 3
+// let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+// let str1 = "hello"
+// let str2 = " talha"
+// let str3 = str1 + str2
+// console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
+
+
+
+
+
+
+
+ 
