a15186e895470fce24440485b084f1a69e51ffd9
talha-015 <muhammdtalha015@gmail.com>
2025-02-27 21:12:26 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,19 @@
+// primitive datatypes:
+// seven types of primitive datatypes 
+// variabbles, string ,null , undefined , bigint ,number, boolean, symbol 
+ 
+const score = "50"
+const scoreValue = 50 
+const  isLoogedIn = false 
+const outsideTemp = null
+let userEmail;
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// refrence (primitive)
+
+// object , array , function 
