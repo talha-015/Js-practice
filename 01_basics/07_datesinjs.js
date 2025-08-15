613b45bcd070ecb74aa0909ecf55f7459d535b56
talha-015 <muhammdtalha015@gmail.com>
2025-08-15 19:59:02 +0000

    date and time

---
+++ 01_basics/07_datesinjs.js
@@ -0,0 +1,6 @@
+let myDate = new Date()
+// console.log(myDate.toString());  
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleTimeString());
+
